b1d4089f7d43efd5bf3ceada4d344339b2096d3f
Viclem <hellooclem.5@gmail.com>
2021-04-24 00:33:06 +0200

    change modal_1_2_ because there used to be no space between the end of the description and the first picture

--- modals.js
+++ modals.js
@@ -1,15 +1,15 @@
 function modal_1_2_(array){
   length = array.length;
-  // str = "<div class=\"row\">"
-  // document.write(str)
+  str = "<div class=\"row\">"
+  document.write(str)
 
   str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
     <img src=\"./images"+array[0]+"\" alt=\"\" class=\"img-fluid\">\
   </div>"
   document.write(str)
 
-  // str = "</div>"
-  // document.write(str)
+  str = "</div>"
+  document.write(str)
 
   str = "<div class=\"row align-items-center\">\
               <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
@@ -21,20 +21,12 @@ function modal_1_2_(array){
              </div></div>";
   document.write(str);
 
-  // if (length > 3){
-  //   str = "<div class=\"row\">"
-  //   document.write(str)
-  // }
   for (var i = 3; i < length; i=i+1){
     str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
       <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
     </div>"
     document.write(str)
   }
-  // if (length > 3){
-  //   str = "</div>"
-  //   document.write(str)
-  // }
 }
 
 
