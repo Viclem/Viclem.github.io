94b8e5aced911ccca61219eb6b5d05b7efbe6375
viclem <hellooclem.5@gmail.com>
2021-04-29 15:03:11 +0200

    modals.js : nouvelles fonctions modals

--- modals.js
+++ modals.js
@@ -1,14 +1,10 @@
+// MODAL _1_2_
 function modal_1_2_(array){
   length = array.length;
-  str = "<div class=\"row\">"
-  document.write(str)
 
-  str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+  str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
     <img src=\"./images"+array[0]+"\" alt=\"\" class=\"img-fluid\">\
-  </div>"
-  document.write(str)
-
-  str = "</div>"
+  </div></div>"
   document.write(str)
 
   str = "<div class=\"row align-items-center\">\
@@ -18,32 +14,30 @@ function modal_1_2_(array){
                <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
                  <img src=\"./images"+array[2]+"\" alt=\"\" class=\"img-fluid\">\
                </div>\
-             </div></div>";
+             </div>";
   document.write(str);
 
   for (var i = 3; i < length; i=i+1){
-    str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
       <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
-    </div>"
+    </div></div>"
     document.write(str)
   }
 }
 
-
+// MODAL _1_
 function modal_1_(array){
   length = array.length;
-  str = "<div class=\"row\">"
-  document.write(str)
+
   for (var i = 0; i < length; i=i+1){
-    str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
       <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
-    </div>"
+    </div></div>"
     document.write(str)
   }
-  str = "</div>"
-  document.write(str)
 }
 
+// MODAL _2_
 function modal_2_(array){
   length = array.length;
   str = "<div class=\"row align-items-center\">\
@@ -56,22 +50,15 @@ function modal_2_(array){
              </div>";
   document.write(str);
 
-  if (length > 2){
-    str = "<div class=\"row\">"
-    document.write(str)
-  }
   for (var i = 2; i < length; i=i+1){
-    str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
       <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
-    </div>"
-    document.write(str)
-  }
-  if (length > 2){
-    str = "</div>"
+    </div></div>"
     document.write(str)
   }
 }
-/*
+
+// MODAL _2_1_1_2_
 function modal_2_1_1_2_(array){
   length = array.length;
   str = "<div class=\"row align-items-center\">\
@@ -84,18 +71,140 @@ function modal_2_1_1_2_(array){
              </div>";
   document.write(str);
 
-  if (length > 2){
-    str = "<div class=\"row\">"
-    document.write(str)
+  for (var i = 2; i < 4; i=i+1){
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+      <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
+    </div></div>";
+    document.write(str);
   }
-  for (var i = 2; i < length; i=i+1){
+
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[4]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[5]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  for (var i = 6; i < length; i=i+1){
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+      <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
+    </div></div>";
+    document.write(str);
+  }
+}
+
+// MODAL 2_1_2_
+function modal_2_1_2_(array){
+  length = array.length;
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[0]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[1]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+    <img src=\"./images"+array[2]+"\" alt=\"\" class=\"img-fluid\">\
+  </div></div>"
+  document.write(str)
+
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[3]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[4]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  for (var i = 5; i < length; i=i+1){
+    str = "<div class=\"row\"><div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+      <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
+    </div></div>";
+    document.write(str);
+  }
+}
+
+// MODAL 3_
+function modal_3_(array){
+  length = array.length;
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[0]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[1]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[2]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  for (var i = 4; i < length; i=i+1){
+    str = "<div class=\"row\">";
+    document.write(str);
     str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
       <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
-    </div>"
-    document.write(str)
+    </div>";
+    document.write(str);
+    str = "</div>";
+    document.write(str);
   }
-  if (length > 2){
-    str = "</div>"
-    document.write(str)
+}
+
+// MODAL 3_1_2_
+function modal_3_1_2_(array){
+  length = array.length;
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[0]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[1]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-4 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[2]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  str = "<div class=\"row\">";
+  document.write(str);
+
+  str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+    <img src=\"./images"+array[3]+"\" alt=\"\" class=\"img-fluid\">\
+  </div>"
+  document.write(str)
+
+  str = "</div>"
+  document.write(str);
+
+  str = "<div class=\"row align-items-center\">\
+              <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[4]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+               <div class=\"col-md-6 col-sm-12 g-3 text-center\">\
+                 <img src=\"./images"+array[5]+"\" alt=\"\" class=\"img-fluid\">\
+               </div>\
+             </div>";
+  document.write(str);
+
+  for (var i = 6; i < length; i=i+1){
+    str = "<div class=\"row\">";
+    document.write(str);
+    str = "<div class=\"col-md-12 col-sm-12 g-3 text-center\">\
+      <img src=\"./images"+array[i]+"\" alt=\"\" class=\"img-fluid\">\
+    </div>";
+    document.write(str);
+    str = "</div>";
+    document.write(str);
   }
-}*/
+}
